4816f857164d359dc1cc3dc46d8ed349e65ff061
Ruzmetov Sunnatillo <143284905+Sunnatillo-Dev@users.noreply.github.com>
2024-01-28 13:34:43 +0500

    g

--- src/components/main/RightContent.tsx
+++ src/components/main/RightContent.tsx
@@ -1,5 +1,5 @@
 import { Box, Text, Flex, Image, Button,  } from '@chakra-ui/react';
-import { React, useState } from 'react';
+import React, { useState } from 'react';
 import PopularUser from './PopularUsers';
 const RightContent:React.FC = () => {    
     const [scrollY, setScrollY] = useState(0)
